82099356ea49109410b174d83e2f9896116f3243
Matthias Portius <Matthias.Portius@gmx.de>
2023-02-16 19:41:53 +0100

    added favicon added styling from old project added responsive register form added other html from old project

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -47,3 +47,13 @@ def create_app(test_config=None):
     # then the index in __init__ and the blog.index (with url_prefix) would be different
     
     return app
+
+# TODO: 
+# A detail view to show a single post. Click a post’s title to go to its page.
+# Comments.
+# Tags. Clicking a tag shows all the posts with that tag.
+# A search box that filters the index page by name.
+# Paged display. Only show 5 posts per page.
+# Upload an image to go along with a post.
+# Format posts using Markdown.
+# An RSS feed of new posts.
---
+++ flaskr/static/script.js
@@ -0,0 +1,25 @@
+// for responsive nav with symbol after
+// function functionnav() {
+//     var x = document.getElementById("IDnav");
+//     if (x.className === "responsive") {
+//         x.className = "";
+//     }
+//     else {
+//         x.className="responsive";
+//     } 
+// }
+
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+        this.classList.toggle("active");
+        var panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
