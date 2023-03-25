aa43117593109d2a53881b44bb5f4cce580f90b7
Matthias Portius <Matthias.Portius@gmx.de>
2023-03-25 17:12:42 +0100

    readme changes

--- README.md
+++ README.md
@@ -16,9 +16,28 @@ For this I looked into [Flask's pattern for file uploads](https://flask.palletsp
 I also added some elements to my index page while looking into the specifics of Flex-Boxes and Responsive Web Design and made some minor changes to the SQL database,
 the config settings of my app and some page's HTML and CSS, added a favicon, etc.
 
+### Example images
 
-## Further plans
+<img src="/Flask_Index.JPG" alt="Index Page" width="100"/>
 
-In the future I plan on adding some elements to my web app like an about me and misc page.
-Adding a `maxlength="200"` property to the textarea of my contact page together with a character count feedback.
+![Index Page](/Flask_Index.JPG)
+![Register Page](/Flask_Register.JPG)
+![Contact Page](/Flask_Contact.JPG)
 
+## Usage
+
+Project installable with `pip install -e`.
+Initialize database with `flask --app flaskr init-db`.  
+Run app with `flask --app flaskr (--debug) run`.
+
+## Further plans and ideas
+
+- "about me" and "misc" page
+- adding a `maxlength="200"` property to the textarea of contact page together with a character count feedback
+- detailed view for single posts
+- comment option for posts
+- tags for posts
+- search box which filters the blog index page
+- page display of blog index page
+- fix upload of multiple files on contact page
+- give blog blueprint an url-prefix like auth and define seperate index in `__init.py__`
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,4 +1,3 @@
-# run app with flask --app flaskr (--debug) run
 import os
 
 from flask import Flask
@@ -27,7 +26,6 @@ def create_app(test_config=None):
     except OSError:
         pass
     
-    # initialize db with "flask --app flaskr init-db" 
     from . import db
     db.init_app(app)
 
@@ -37,24 +35,11 @@ def create_app(test_config=None):
     from . import blog
     app.register_blueprint(blog.bp)
     app.add_url_rule("/", endpoint="index")
-    # associates endpoint name "index" with "/" url so thath url_for("index") works and generates "/"
-    # without this only url_for("blog.index") would work
-    # app.route("/") def index would do the same
-    # could also give blog Blueprint a url_prefix like auth and define seperate index view in the application factory (__init__)
-    # then the index in __init__ and the blog.index (with url_prefix) would be different
+    # associates endpoint name "index" with "/" url so thath url_for("index") generates "/"
+    # without this only url_for("blog.index") would generate "/" url
     
     from . import contact
     app.register_blueprint(contact.bp)
     app.add_url_rule("/contact", endpoint="contact")
 
     return app
-
-# TODO: 
-# A detail view to show a single post. Click a post’s title to go to its page.
-# Comments under posts.
-# Tags. Clicking a tag shows all the posts with that tag.
-# A search box that filters the index page by name.
-# Paged display. Only show 5 posts per page.
-# Upload an image to go along with a post.
-# Format posts using Markdown.
-# an about me
